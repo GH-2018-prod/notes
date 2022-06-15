121952ef5002fdc008e7cab2ac1e28ba0423bfee
GH-2018-prod <maancuza@gmail.com>
2022-06-14 20:37:46 -0600

    changing proyect to notes

---
+++ scripts/app.js
@@ -0,0 +1,66 @@
+document.getElementById('formTask').addEventListener('submit',save);
+window.onload = getTasks;
+function save(e){
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    executeSave(title, description, 'tasks');
+    document.getElementById('formTask').reset();
+    getTasks();
+}
+function executeSave(title, description, savedIssues){
+    const task = {
+        title,
+        description
+    }
+    if(localStorage.getItem(savedIssues) === null){
+        let resolvedIssues = [];
+        resolvedIssues.push(task);
+        localStorage.setItem(savedIssues, JSON.stringify(resolvedIssues));
+    } else {
+        let resolvedIssues = JSON.parse(localStorage.getItem(savedIssues));
+        resolvedIssues.push(task);
+        localStorage.setItem(savedIssues, JSON.stringify(resolvedIssues));
+    }
+}
+function getTasks(){
+    const buttonClass = 'btn btn-warning';
+    const tasksToFind = 'tasks';
+    const tasks = JSON.parse(localStorage.getItem(tasksToFind));
+    const viewTasks = document.getElementById('tasks');
+
+    printIssues(tasks, viewTasks, tasksToFind, buttonClass);
+}
+function printIssues(tasks, viewTasks, tasksToFind, buttonClass){
+    viewTasks.innerHTML = '';
+    for(let i = 0; i<tasks.length; i++){
+        const title = tasks[i].title;
+        const description = tasks[i].description;
+        viewTasks.innerHTML += `
+            <div data-row="${title}" id="sadd" class="card mb-4 mt-2">
+                <div class="card-header">
+                    <p>${title}</p>   
+                </div>
+                <div class="card-body">
+                    <p> ${description}</p>
+                </div>
+                <a class="${buttonClass}" onclick="resolveIssue('${title}','${tasksToFind}')">
+                    Borrar
+                </a>
+            </div>
+        `;
+    }
+}
+function resolveIssue(title, tasksToFind){
+    let message = 'This action will send the ticket to resolved issues queue. Proceed with ticket: ? ';
+    let tasks = JSON.parse(localStorage.getItem(tasksToFind));
+    for(let i=0; i<tasks.length; i++){
+        if(tasks[i].title == title){
+            if(window.confirm(message + title)){
+                tasks.splice(i, 1);
+                localStorage.setItem(tasksToFind, JSON.stringify(tasks));
+            }
+        }
+    } 
+    getTasks();
+}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-
-async function main(){
-    await app.listen(app.get('port'))
-    console.log('Hello there! this is port: ', app.get('port'));
-}
-main();
